0b3579903f69cd30db98838b7beade80ea194535
Denis <patron.2001den@mail.ru>
2023-02-28 00:06:57 +0700

    Send form ajax

--- dist/js/script.js
+++ dist/js/script.js
@@ -183,7 +183,17 @@ document.addEventListener('DOMContentLoaded', function () {
             formInput = 0;
             validateInputs();
             if (formSuccess == formInput) {
-                // form.submit();
+                $.ajax({
+                    type: "POST",
+                    url: "mailer/smart.php",
+                    data: $(this).serialize()
+                }).done(function () {
+                    $(this).find("input").val("");
+                    $('form').trigger('reset');
+                    $('form input').removeClass('success');
+                    $('.form-main__message').addClass('active');
+                });
+                return false;
             }
         })
 
